29c23ecae22af0b2bc38a6a4ac9aafda422be6ac
ezu0 <h-ezure@est.co.jp>
2022-02-21 18:19:37 +0900

    sec12-finish

--- MySample/Program.cs
+++ MySample/Program.cs
@@ -9,23 +9,46 @@ namespace MySample
     class Program
     {
         static void Main(string[] args)
+        //クラスリストとLINQ
+        //リストと配列の使い開け
+        //通常はリストを使う、要素の追加削除などを行わない場合は配列をつかうといい
         {
-            //C#の方はすべて値型と参照型のどっちか
-            //列挙型
+            var lines = new List<string>
+            {
+                "おはよう",
+                "こんにちは",
+                "こんばんは"
+            };
+            lines.Add("おやすみ");
+            lines.Add("さようなら");
+            /*            foreach (var s in lines)
+                        {
+                            Console.WriteLine(s);
+                        }*/
 
-            var week = DayOfWeek.Sunday;
-            if (week == DayOfWeek.Friday)
+            var messages = lines;
+            messages.RemoveAt(2);
+            foreach (var t in messages)
             {
-                Console.WriteLine("金曜日です");
-            } else
-            {
-                Console.WriteLine("金曜日ではありません");
+                Console.WriteLine(t);
             }
+            var count = messages.Count;
+            Console.WriteLine($"要素数：{count}");
 
-           
         }
-
-
-
+        class book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int Pages { get; set; }
+            public int Rating { get; set; }
+            public Book(string title, string author, int pages, int rating)
+            {
+                Title = title;
+                Author = author;
+                Pages = pages;
+                Rating = rating;
+            }
+        }
     }
 }
