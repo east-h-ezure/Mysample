d1e1928cdc00efa731d21a9efad0bf5fca90727b
ezu0 <h-ezure@est.co.jp>
2022-02-21 16:33:03 +0900

    sec11-sinish

--- MySample/Program.cs
+++ MySample/Program.cs
@@ -10,6 +10,18 @@ namespace MySample
     {
         static void Main(string[] args)
         {
+            //C#の方はすべて値型と参照型のどっちか
+            //列挙型
+
+            var week = DayOfWeek.Sunday;
+            if (week == DayOfWeek.Friday)
+            {
+                Console.WriteLine("金曜日です");
+            } else
+            {
+                Console.WriteLine("金曜日ではありません");
+            }
+
            
         }
 
